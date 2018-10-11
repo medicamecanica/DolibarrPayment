3d2fdac1304d16f3b8e855ff173f27f7126973e3
Ander <npander@hotmail.com>
2018-10-11 15:00:49 -0500

    check push

--- src/main/java/co/medicamecanica/dolibarrpayment/InvoiceDetailActivity.java
+++ src/main/java/co/medicamecanica/dolibarrpayment/InvoiceDetailActivity.java
@@ -70,6 +70,7 @@ public class InvoiceDetailActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
         if (id == android.R.id.home) {
+            
             // This ID represents the Home or Up button. In the case of this
             // activity, the Up button is shown. Use NavUtils to allow users
             // to navigate up one level in the application structure. For
